76ffc24d07ce0843c52cdd91329d9d35963ba2f1
Peter Marsh <pmarsh@scottlogic.com>
2024-04-16 11:23:16 +0100

    wrap in app div

--- ui/src/main.tsx
+++ ui/src/main.tsx
@@ -6,6 +6,8 @@ import './styles.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
 	<React.StrictMode>
-		<App />
+		<div className="app">
+			<App />
+		</div>
 	</React.StrictMode>
 )
