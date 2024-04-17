c53f4a4e55edbf989397abeb87adee763f53be95
Peter Marsh <pmarsh@scottlogic.com>
2024-04-17 11:41:26 +0100

    display and format to do list

--- ui/src/Todolist/DisplayList.tsx
+++ ui/src/Todolist/DisplayList.tsx
@@ -1,10 +1,22 @@
 import { TodoItem } from './types'
+import './DisplayList.scss'
+
+const Separator = () => <span className="separator">{'|'}</span>
 
 function DisplayList({ todoItems }: { todoItems: TodoItem[] }) {
 	return (
 		<ul>
 			{todoItems.map((todoItem, index) => (
-				<li key={index}>{todoItem.title}</li>
+				<li key={index}>
+					<input type="checkbox" checked={todoItem.complete} />
+					<p>
+						<span className="title">{todoItem.title}</span>
+						<Separator />
+						<span>{todoItem.location}</span>
+						<Separator />
+						<span>{todoItem.dueDate.toDateString()}</span>
+					</p>
+				</li>
 			))}
 		</ul>
 	)
