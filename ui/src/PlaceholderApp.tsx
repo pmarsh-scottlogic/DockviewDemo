dd6952f329dfff2c112cf134cd94fe02f6a46004
Peter Marsh <pmarsh@scottlogic.com>
2024-04-17 10:47:55 +0100

    adds basic placeholder to do list

--- ui/src/PlaceholderApp.tsx
+++ ui/src/PlaceholderApp.tsx
@@ -1,5 +1,54 @@
+import { useState } from 'react'
+import './Placeholder.css'
+import { TodoItem } from './Todolist/types'
+
+const PLACEHOLDER_TODO_ITEMS = [
+	{
+		title: 'Buy groceries',
+		location: 'Tesco',
+		complete: false,
+		dueDate: new Date(Date.parse('01 Sep 2012 00:00:00 GMT')),
+	},
+	{
+		title: 'Cook curry',
+		location: 'Home',
+		complete: false,
+		dueDate: new Date(Date.parse('02 Sep 2012 00:00:00 GMT')),
+	},
+	{
+		title: 'Recycle batteries',
+		location: 'Tesco',
+		complete: false,
+		dueDate: new Date(Date.parse('01 Sep 2012 00:00:00 GMT')),
+	},
+	{
+		title: 'Take bins out',
+		location: 'Home',
+		complete: false,
+		dueDate: new Date(Date.parse('03 Sep 2012 00:00:00 GMT')),
+	},
+	{
+		title: 'Take kids to school',
+		location: 'School',
+		complete: true,
+		dueDate: new Date(Date.parse('01 Sep 2012 00:00:00 GMT')),
+	},
+] as TodoItem[]
+
 function PlaceholderApp() {
-	return <p>Hey</p>
+	const [todoItems, setTodoItems] = useState<TodoItem[]>(
+		PLACEHOLDER_TODO_ITEMS
+	)
+	return (
+		<section>
+			<h1>To do list</h1>
+			<ul>
+				{todoItems.map((todoItem, index) => (
+					<li key={index}>{todoItem.title}</li>
+				))}
+			</ul>
+		</section>
+	)
 }
 
 export default PlaceholderApp
