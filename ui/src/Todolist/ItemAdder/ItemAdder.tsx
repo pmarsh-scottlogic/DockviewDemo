21fbd1155a2625c1e74fc2b0d5c07370eab8adb5
Peter Marsh <pmarsh@scottlogic.com>
2024-04-17 14:10:23 +0100

    fills out item adder

--- ui/src/Todolist/ItemAdder/ItemAdder.tsx
+++ ui/src/Todolist/ItemAdder/ItemAdder.tsx
@@ -1,7 +1,26 @@
 import './ItemAdder.scss'
 
 function ItemAdder() {
-	return <p>ItemAdder</p>
+	return (
+		<div>
+			<table>
+				<tr>
+					<label htmlFor="title">Title</label>
+					<input id="title" type="text" />
+				</tr>
+				<tr>
+					<label htmlFor="location">Location</label>
+					<input id="location" type="text" />
+				</tr>
+				<tr>
+					<label htmlFor="dueDate">Due date</label>
+					<input type="datetime-local" id="dueDate" />
+				</tr>
+			</table>
+
+			<button>Add to list</button>
+		</div>
+	)
 }
 
 export default ItemAdder
