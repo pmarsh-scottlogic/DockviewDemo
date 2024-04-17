39d77303a4ed0e986bb3f8f8ecb2b0cbaae6c4d4
Peter Marsh <pmarsh@scottlogic.com>
2024-04-17 15:49:38 +0100

    adds checkboxes for to do and complete

--- ui/src/Todolist/ListDisplayer/FilterPanel.tsx
+++ ui/src/Todolist/ListDisplayer/FilterPanel.tsx
@@ -1,7 +1,26 @@
 import './FilterPanel.scss'
 
 function FilterPanel() {
-	return <div className="filterPanel">filters</div>
+	return (
+		<div className="filterPanel">
+			<div className="labels">
+				<label>completedness</label>
+				<label>location</label>
+			</div>
+			<div className="inputs">
+				<div className="completednessPanel">
+					<label>
+						to do
+						<input type="checkbox" />
+					</label>
+					<label>
+						complete
+						<input type="checkbox" />
+					</label>
+				</div>
+			</div>
+		</div>
+	)
 }
 
 export default FilterPanel
