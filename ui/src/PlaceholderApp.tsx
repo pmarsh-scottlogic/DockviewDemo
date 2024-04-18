67d87b9baa4491e2240d13b3305a8a8283f1a1b0
Peter Marsh <pmarsh@scottlogic.com>
2024-04-18 09:28:50 +0100

    moves placeholder items to fresh file

--- ui/src/PlaceholderApp.tsx
+++ ui/src/PlaceholderApp.tsx
@@ -3,39 +3,7 @@ import './Placeholder.css'
 import { TodoItem } from './Todolist/types'
 import ListDisplayer from './Todolist/ListDisplayer/ListDisplayer'
 import ItemAdder from './Todolist/ItemAdder/ItemAdder'
-
-const PLACEHOLDER_TODO_ITEMS = [
-	{
-		title: 'Buy groceries',
-		location: 'Tesco',
-		complete: false,
-		dueDate: new Date(Date.parse('01 Sep 2012 00:00:00 GMT')),
-	},
-	{
-		title: 'Cook curry',
-		location: 'Home',
-		complete: false,
-		dueDate: new Date(Date.parse('02 Sep 2012 00:00:00 GMT')),
-	},
-	{
-		title: 'Recycle batteries',
-		location: 'Tesco',
-		complete: false,
-		dueDate: new Date(Date.parse('01 Sep 2012 00:00:00 GMT')),
-	},
-	{
-		title: 'Take bins out',
-		location: 'Home',
-		complete: false,
-		dueDate: new Date(Date.parse('03 Sep 2012 00:00:00 GMT')),
-	},
-	{
-		title: 'Take kids to school',
-		location: 'School',
-		complete: true,
-		dueDate: new Date(Date.parse('01 Sep 2012 00:00:00 GMT')),
-	},
-] as TodoItem[]
+import PLACEHOLDER_TODO_ITEMS from './Todolist/placeholderItems'
 
 function PlaceholderApp() {
 	const [todoItems, setTodoItems] = useState<TodoItem[]>(
