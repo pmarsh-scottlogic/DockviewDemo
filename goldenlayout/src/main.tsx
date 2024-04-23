6d2e139355cc581c95fedf0462764073966d00dc
Peter Marsh <pmarsh@scottlogic.com>
2024-04-23 13:45:08 +0100

    back to minimal app

--- goldenlayout/src/main.tsx
+++ goldenlayout/src/main.tsx
@@ -1,7 +1,5 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import 'dockview/dist/styles/dockview.css'
-import './styles.css'
 import App from './App.tsx'
 import { store } from './Todolist/store.ts'
 import { Provider } from 'react-redux'
