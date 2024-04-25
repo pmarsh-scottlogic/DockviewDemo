0ac693166d8945208cfc410ec83ca4ea3b6c9154
Peter Marsh <pmarsh@scottlogic.com>
2024-04-25 17:04:37 +0100

    float experiments

--- flexLayout/src/App.tsx
+++ flexLayout/src/App.tsx
@@ -50,19 +50,75 @@ const json: IJsonModel = {
 					},
 				],
 			},
+			{
+				type: 'tabset',
+				weight: 50,
+				children: [
+					{
+						type: 'tab',
+						name: 'derek',
+						component: 'float',
+					},
+				],
+			},
 		],
 	},
 }
 
-const model = Model.fromJson(json)
-
 function App() {
+	let model = Model.fromJson(json)
+
 	const factory = (node: TabNode) => {
 		const component = node.getComponent()
 		return component === 'todoDisplay' ? (
 			<ListDisplayer />
 		) : component === 'todoAdder' ? (
 			<ItemAdder />
+		) : component === 'float' ? (
+			<div>
+				<p>I'm a floater</p>
+				<button
+					onClick={() => {
+						console.log(
+							JSON.stringify(node.getModel().toJson(), null, 2)
+						)
+						const sameAsStart =
+							JSON.stringify(json) ===
+							JSON.stringify(node.getModel().toJson())
+						console.log('same as start', sameAsStart)
+					}}
+				>
+					show json
+				</button>
+				<button
+					onClick={() => {
+						const modelJson = JSON.stringify(
+							node.getModel().toJson().layout,
+							undefined,
+							2
+						)
+						localStorage.setItem('savedLayout', modelJson)
+					}}
+				>
+					save
+				</button>
+				<button
+					onClick={() => {
+						const modelJson = localStorage.getItem('savedLayout')
+						console.log(
+							'getting saved layout from localStorage',
+							modelJson
+						)
+						if (modelJson) {
+							const JsonObj: IJsonModel = JSON.parse(modelJson)
+							model = Model.fromJson(JsonObj)
+						}
+					}}
+				>
+					load
+				</button>
+				<button>new window</button>
+			</div>
 		) : (
 			<p>{`DevError: the component ${component} was not defined in the factory`}</p>
 		)
